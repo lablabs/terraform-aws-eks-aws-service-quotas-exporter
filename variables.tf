fd93608747784080eb054054962cd4261281622f
Martin Odstrčilík <martin.odstrcilik@lablabs.io>
2024-11-14 08:45:31 +0100

    feat: prepare addon

--- variables.tf
+++ variables.tf
@@ -4,3 +4,9 @@ variable "enabled" {
   default     = true
   description = "Set to false to prevent the module from creating any resources."
 }
+
+variable "exporter_config" {
+  type        = any
+  default     = {}
+  description = "The configuration for the exporter, see https://github.com/lablabs/aws-service-quotas-exporter/blob/main/config/example.yaml"
+}
